156b980320476b9f7bc7c1638f517cc827ccbf87
royrover <60468002+royrover@users.noreply.github.com>
2025-09-11 08:45:19 +0700

    Update dbidhl_update.py

--- scripts/highlight_football/dbidhl_update.py
+++ scripts/highlight_football/dbidhl_update.py
@@ -147,7 +147,7 @@ for page in range(start_page, end_page + 1):
 
 # ✅ อัปเดตข้อมูล
 data["stations"] = new_stations + stations_list
-data["author"] = f"update {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}"
+data["author"] = f"update {now_th.strftime('%d-%m-%Y %H:%M:%S')}"
 
 # ✅ เขียนไฟล์ JSON (.w3u)
 with open(json_file, 'w', encoding='utf-8') as file:
@@ -165,5 +165,6 @@ with open(m3u_file, 'w', encoding='utf-8') as f:
     f.write(m3u_content)
 
 print(f"✅ บันทึกไฟล์ {json_file} และ {m3u_file} เรียบร้อยแล้ว")
+
 
 
